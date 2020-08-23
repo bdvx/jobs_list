8eca2f447e2bf539038aad58d0742d815e1e5a53
Bdvx <bdv2507@gmail.com>
2020-08-24 02:24:16 +0300

    hooked up redis to Github Jobs

--- worker/tasks/fetch-github-jobs.js
+++ worker/tasks/fetch-github-jobs.js
@@ -1,5 +1,11 @@
 var fetch = require('node-fetch');
 
+const redis = require("redis");
+const client = redis.createClient();
+
+const { promisify } = require("util");
+// const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
 
 const baseURL = 'https://jobs.github.com/positions.json';
 
@@ -23,9 +29,10 @@ async function fetchGithubJobs() {
         page++;
     }
 
-    console.log('allJobs', allJobs);
+    
 
-
+    const successResult = await setAsync('githubJobs', JSON.stringify(allJobs));
+    console.log('allJobs', {successResult});
 }
 
 fetchGithubJobs();
